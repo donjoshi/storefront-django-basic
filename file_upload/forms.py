2b48098ff8314e31ba90d4d3fcd4ade8baf94aa5
Don Joshi <donjneelamkavil@gmail.com>
2023-06-25 13:18:21 +0530

    committed

--- file_upload/forms.py
+++ file_upload/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import File
 
 class FileForm(forms.ModelForm):
+    
     class Meta:
         model = File
         fields = ('pdf',)
--- file_upload/models.py
+++ file_upload/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 
 class File(models.Model):
-    pdf=models.FileField(upload_to='pdf/')
-    uploaded_at=models.DateTimeField(auto_now_add=True)
+    # title = models.CharField(max_length=100)
+    pdf=models.FileField(upload_to='pdfs/')
+    # uploaded_at=models.DateTimeField(auto_now_add=True)
